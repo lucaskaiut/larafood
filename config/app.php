1e37d8f27f3b8c9d4e2c18a056b3ca17d2581d51
Lucas Kauit <lucas@onway.com.br>
2020-11-27 19:34:29 -0300

    fixed timezone setting

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Amercia/Sao_Paulo',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
