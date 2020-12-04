9379bf2fc0cb06ebdd7ec61a7f6faefadf98bcd6
Lucas Kauit <lucas@onway.com.br>
2020-12-04 20:48:49 -0300

    finished user register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -50,9 +51,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            /*'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],*/
         ]);
     }
 
@@ -64,10 +65,26 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        if(!$plan = session('plan'))
+            return redirect()
+                ->route('site.home')
+                ->with('error', 'Algo deu errado. Tente novamente mais tarde');
+
+        $tenant = $plan->tenants()->create([
+            'cnpj' => $data['cnpj'],
+            'name' => $data['company'],
+            'url' => Str::kebab($data['company']),
+            'email' => $data['email'],
+            'subscription' => now(),
+            'expires_at' => now()->addDays(7)
+        ]);
+
+        $user = $tenant->users()->create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password'])
         ]);
+
+        return $user;
     }
 }
