26fc725686c7a61562ef5d82df66e3505b9b14d2
Lucas Kauit <lucas@onway.com.br>
2020-12-04 21:27:55 -0300

    added uuid column for tenants table

--- app/Services/TenantService.php
+++ app/Services/TenantService.php
@@ -24,7 +24,6 @@ class TenantService{
         $tenant = $this->plan->tenants()->create([
             'cnpj' => $this->data['cnpj'],
             'name' => $this->data['company'],
-            'url' => Str::kebab($this->data['company']),
             'email' => $this->data['email'],
             'subscription' => now(),
             'expires_at' => now()->addDays(7)
