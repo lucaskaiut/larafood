5efaafa604971eee0b734ec4721d3a9c356edec1
Lucas Kauit <lucas@onway.com.br>
2020-12-10 21:42:59 -0300

    finished new acl control

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -4,16 +4,46 @@
 namespace App\Models\Traits;
 
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
     public function permissions(): array {
-        $tenant = $this->tenant;
-        $plan = $tenant->plan;
-        $profiles = $plan->profiles;
+        $permissionsPlan = $this->permissionsPlan();
+
+        $permissionsRole = $this->permissionsRole();
 
         $permissions = [];
-        foreach($profiles as $profile){
+
+        foreach($permissionsRole as $permissionRole){
+            if(in_array($permissionRole, $permissionsPlan))
+                array_push($permissions, $permissionRole);
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan(): array {
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
+        $plan = $tenant->plan;
+
+        $permissions = [];
+        foreach($plan->profiles as $profile){
             foreach($profile->permissions as $permission){
+                array_push($permissions, $permission->name);
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsRole(): array {
+        $roles = $this->roles;
+
+        $permissions = [];
+
+        foreach($roles as $role){
+            foreach($role->permissions as $permission){
                 array_push($permissions, $permission->name);
             }
         }
