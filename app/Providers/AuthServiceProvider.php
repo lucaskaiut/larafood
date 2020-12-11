42e629f9ee7494bccb12e2e2be12421d4e17ea02
Lucas Kauit <lucas@onway.com.br>
2020-12-10 21:09:31 -0300

    fixed system to use App\Models\User instead of App\User

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Permission;
-use App\User;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
